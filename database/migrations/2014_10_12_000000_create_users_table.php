dd0451386a0e808637297eb3b0746ce32dba530b
Agustin <agus.jimenez.caba@gmail.com>
2020-12-23 23:49:22 -0300

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create( (new User)->getTable() , function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 120);
+            $table->string('email', 90)->unique();
+            $table->string('image', 160);
+            $table->string('utr', 12);
+            $table->string('password');
+            $table->boolean('enabled')->default(false);
+            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists( (new User)->getTable() );
+    }
+}
---
+++ database/migrations/2020_12_24_023359_create_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Company;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create( (new Company)->gettable() , function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 70);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists( (new Company)->gettable() );
+    }
+}
